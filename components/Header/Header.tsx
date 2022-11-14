68e4c8608af9ba00fa9dafcbe2b69715480866c9
sigristarisa <arisasigrist.ch@gmail.com>
2022-11-14 17:35:00 +0100

    installed classnames and attempting conditional rendering

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import { NavItem } from "../../types/types";
 import headerStyles from "./Header.module.css";
+import cn from "classnames";
 
 interface Props {
   setHeaderText: Function;
   getNavId: Function;
+  activateNav: Function;
 }
 
-const Header = ({ setHeaderText, getNavId }: Props) => {
+const Header = ({ setHeaderText, getNavId, activateNav }: Props) => {
   const navArr: NavItem[] = [
     {
       id: 0,
@@ -29,7 +31,16 @@ const Header = ({ setHeaderText, getNavId }: Props) => {
       <nav>
         <ul className='three-grid-columns place-items_center'>
           {navArr.map((nav: NavItem, index: number) => (
-            <li key={index} onClick={() => setHeaderText(nav.headerText)}>
+            <li
+              className={`${headerStyles.item} ${cn({
+                active: activateNav(index),
+              })}`}
+              key={index}
+              onClick={() => {
+                setHeaderText(nav.headerText);
+                getNavId(nav.id);
+              }}
+            >
               <Link className={headerStyles.navItem} href={nav.navigate}>
                 {nav.navContent}
               </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,36 @@
 import { useState } from "react";
 import Header from "../components/Header/Header";
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   const [navId, setNavId] = useState<number>(0);
   const [headerText, setHeaderText] = useState<string>("Mayonnaise Of The Day");
 
   const getNavId = (btnId: number): void => setNavId(btnId);
-  const activateNav = (index: number): string => {
-    return navId === index ? "active" : "inactive";
+  const activateNav = (index: number): boolean => {
+    return navId === index;
   };
 
+  console.log(navId);
+
   return (
-    <div>
+    <div className='justify-items_center'>
+      <Head>
+        <title>MOTD – Mayonnaise of the Day</title>
+      </Head>
       <Header
         setHeaderText={setHeaderText}
         getNavId={getNavId}
-        // activateNav={activateNav}
+        activateNav={activateNav}
       />
       <p>{headerText}</p>
+      <Image
+        src='/images/mayonnaise-lid.png'
+        width={900}
+        height={300}
+        alt='mayonnaise lid'
+      />
     </div>
   );
 }
