37c162f0dbbeb31f3ce63e199e91e9eaeff7f800
sigristarisa <arisasigrist.ch@gmail.com>
2022-11-16 16:52:03 +0100

    adjusted About page layout

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,32 +4,34 @@ import styles from "../styles/About.module.css";
 
 const About = () => {
   return (
-    <div className={`${styles.about_container} place-items_center`}>
+    <div>
       <Header />
-      <MainTitle headerText={""} />
-      <h2>
-        By the Mayo-Lover,<br></br> For the Mayo-Lover, <br></br>Of the
-        Mayo-Lover.
-      </h2>
-      <div className={`${styles.about_text_container}`}>
-        <p>
-          You might go along with a little bit of mayonnaise on toast,<br></br>{" "}
-          but are you ready for mayonnaise on your rice,<br></br> your steamed
-          vegetables, your potato chips?
-        </p>
-        <p>
-          To expand the potential of mayonnaise,<br></br> MOTD will select your
-          mayonnaise of the day – out of 100 variations!
-        </p>
+      <main className={`${styles.about_container} place-items_center`}>
+        <MainTitle
+          headerText={
+            "By the Mayo-Lover, For the Mayo-Lover, Of the Mayo-Lover"
+          }
+        />
+        <div className={`${styles.about_text_container}`}>
+          <p>
+            You might go along with a little bit of mayonnaise on toast,
+            <br></br> but are you ready for mayonnaise on your rice,<br></br>{" "}
+            your steamed vegetables, your potato chips?
+          </p>
+          <p>
+            To expand the potential of mayonnaise,<br></br> MOTD will select
+            your mayonnaise of the day – out of 100 variations!
+          </p>
 
-        <p>
-          But be careful – you can only get one mayonnaise per day<br></br> to
-          prevent your mayo-addiction.
+          <p>
+            But be careful – you can only get one mayonnaise per day<br></br> to
+            prevent your mayo-addiction.
+          </p>
+        </div>
+        <p className={`${styles.creator_text}`}>
+          creator: <a href='https://github.com/sigristarisa'>Arisa Sigrist</a>
         </p>
-      </div>
-      <p className={`${styles.creator_text}`}>
-        creator: <a href='https://github.com/sigristarisa'>Arisa Sigrist</a>
-      </p>
+      </main>
     </div>
   );
 };
